1636000e95586d47f70af900b65747f6663938c5
myokmydj <158419591+myokmydj@users.noreply.github.com>
2026-01-03 00:39:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { appendMediaToMessage } from '../../../../script.js';
 import { regexFromString } from '../../../utils.js';
 import { SlashCommandParser } from '../../../slash-commands/SlashCommandParser.js';
 
-const extensionName = 'IAGF';
+const extensionName = 'I-AGF';
 const extensionFolderPath = `/scripts/extensions/third-party/${extensionName}`;
 
 let TagMatcher = null;
@@ -2464,3 +2464,4 @@ window.imageAutoGeneration = {
     getNAIExtraParams,
     detectCharacterFromPrompt,
 };
+
